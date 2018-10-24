d40cbba16245d62ae33a13183bdf7d7e9c76c01c
npool9 <npool@elon.edu>
2018-10-24 17:41:31 -0400

    Far from complete, but rough visualization of dataset

--- UnitConversion.py
+++ UnitConversion.py
@@ -3,14 +3,14 @@
 
 # # Unit Conversion
 
-# In[35]:
+# In[1]:
 
 
 import pandas as pd
 import string
 
 
-# In[36]:
+# In[2]:
 
 
 """
@@ -63,7 +63,7 @@ def convert_to_ml(num, unit):
     return(output)
 
 
-# In[37]:
+# In[3]:
 
 
 """
@@ -109,135 +109,153 @@ def convert_to_grams(num, unit):
 
 # ## Final Conversion Function
 
-# In[38]:
+# In[11]:
 
 
 """
 Convert the unit, and let the code decide whether the measurement is
 volume or mass
 """
-def convert(num, unit, df=None, i=None):
-    # Eliminate punctuation and notion of case
-    table = str.maketrans({key: None for key in string.punctuation})
-    unit = unit.translate(table).lower().strip()
+def convert(num, unit, ingred):
     the_unit = ""
     weight = 0
     volume = False
     mass = False
+    quantity = False
     
     # Testing for volume
-    if "gal" in unit:
+    if "gal " in unit or 'gallon ' in unit:
         the_unit = "gallon"
         weight = 3785.41
         volume = True
-    elif "qu" in unit or "qt" in unit or "q" in unit:
+    elif "quart " in unit or "qt " in unit or "q " in unit:
         the_unit = "quart"
         weight = 946.353
         volume = True
-    elif "pt" in unit or "pint" in unit:
+    elif "pt " in unit or "pint " in unit:
         the_unit = "pint"
         weight = 473.176
         volume = True
-    elif "cup" in unit or 'c' == unit:
+    elif "cup " in unit or 'c ' == unit:
         the_unit = "cup"
         weight = 240
         volume = True
-    elif "fl" in unit or "oz" in unit:
+    elif "fl " in unit or "oz " in unit:
         the_unit = "fl oz"
         weight = 29.5735
         volume = True
-    elif "tbsp" in unit or "table" in unit:
+    elif "tbsp " in unit or "table " in unit:
         the_unit = "tablespoon"
         weight = 14.7868
         volume = True
-    elif "tsp" in unit or "tea" in unit:
+    elif "tsp " in unit or "tea " in unit:
         the_unit = "teaspoon"
         weight = 4.92892
         volume = True
-    elif "m3" in unit or "cubic meter" in unit or "meter" in unit:
+    elif "m3 " in unit or "cubic meter " in unit or "meter " in unit:
         the_unit = "cubic meter"
         weight = 1e6
         volume = True
-    elif "ml" in unit or "milli" in unit:
+    elif "ml " in unit or "milli " in unit:
         the_unit = "milliliter"
         weight = 1
         volume = True
-    elif "liter" in unit or "l" == unit:
+    elif "liter " in unit or "l " == unit:
         the_unit = "liter"
         weight = 1000
         volume = True
-    elif "cubic foot" in unit or "f3" in unit or "feet" in unit:
+    elif "cubic foot " in unit or "f3 " in unit or "feet " in unit:
         the_unit = "cubic foot"
         weight = 28316.8
         volume = True
-    elif "cubic inch" in unit or "in3" in unit or "inch" in unit:
+    elif "cubic inch " in unit or "in3 " in unit or "inch " in unit:
         the_unit = "cubic inch"
         weight = 16.3871
         volume = True
+    elif "pinch " in unit or "bit " in unit: # extraneous cases
+        # Vocab words that we'll estimate to half a teaspoon
+        the_unit = "teaspoon"
+        num = 0.5
+        weight = 4.92892
       
     # Testing for mass
-    elif "metric ton" in unit or "mt" in unit or "m t" in unit:
+    elif "metric ton" in unit or "mt " in unit or "m t " in unit:
         the_unit = "metric ton"
         weight = 1e6
         mass = True
-    elif "kilo" in unit or "kg" in unit:
+    elif "kilo" in unit or "kg " in unit:
         the_unit = "kilogram"
         weight = 1000
         mass = True
-    elif "gram" in unit or "g" == unit:
+    elif "gram" in unit or "g " == unit:
         the_unit = "gram"
         weight = 1
         mass = True
-    elif "milli" in unit or "mg" in unit:
+    elif "milli" in unit or "mg " in unit:
         the_unit = "milligram"
         weight = 0.001
         mass = True
-    elif "micro" in unit or "μg" in unit or "mcg" in unit:
+    elif ("micro" in unit and "wave" not in unit) or "μg " in unit or "mcg " in unit:
         the_unit = "microgram"
         weight = 1e-6
         mass = True
-    elif "ton" in unit or "t" == unit:
+    elif "ton " in unit or "t " == unit:
         the_unit = "ton"
         weight = 907185
         mass = True
-    elif "stone" in unit or "st" == unit:
+    elif "stone " in unit or "st " == unit:
         the_unit = "stone"
         weight = 6350.29
         mass = True
-    elif "pound" in unit or "lb" in unit:
+    elif "pound " in unit or "lb " in unit:
         the_unit = "pound"
         weight = 453.592
         mass = True
-    elif "ounce" in unit or "ozs" in unit or "oz" in unit:
+    elif "ounce " in unit or "ozs " in unit or "oz " in unit:
         the_unit = "ounces"
         weight = 28.3495
         mass = True
+    else: # if no match, we express the ingredient as a quantity instead of a unit of measure
+        quantity = True
+        weight = 1
         
     # Convert the unit
     output = round(weight * num, 3)
     if volume:
         print('Converted', the_unit, 'to milliliters')
-        if i != None:
-            df.loc[i, 'Volume'] = output
-            df.loc[i, 'Mass'] = 0
+        mass = 0
+        volume = output
+        cols = [ingred + '_mass', ingred + '_volume']
+        measurements = [mass, volume]
     elif mass:
         print('Converted', the_unit, 'to grams')
-        if i != None:
-            df.loc[i, 'Mass'] = output
-            df.loc[i, 'Volume'] = 0
-    print(output)
-    return(output)
+        mass = output
+        volume = 0
+        cols = [ingred + '_mass', ingred + '_volume']
+        measurements = [mass, volume]
+    elif quantity:
+        cols = [unit]
+        measurements = [output]
+        print('Ingredient expressed as quantity')
+    # print(output)
+    return(measurements, cols, quantity)
+
+
+# In[5]:
+
+
+#convert(0.5, 'cup', 'wheat_germ')
 
 
 # ## Conversion Testing
 
-# In[39]:
+# In[6]:
 
 
 import random
 
 
-# In[40]:
+# In[7]:
 
 
 # List of some expected input units to the function
@@ -257,19 +275,21 @@ nums = [random.randint(1, 1000) for i in range(tests)]
 indices = [random.randint(0, len(units) - 1) for i in range(tests)]
 
 
-# In[41]:
+# In[8]:
 
 
-df = pd.DataFrame(columns=["Volume", "Mass"])
-for i in range(tests):
-    print("INPUT     Num:", nums[i], ', Indices:', units[indices[i]])
-    convert(nums[i], units[indices[i]], df=df, i=i)
-    print()
+def test():
+    df = pd.DataFrame(columns=["Volume", "Mass"])
+    for i in range(tests):
+        print("INPUT     Num:", nums[i], ', Indices:', units[indices[i]])
+        convert(nums[i], units[indices[i]], df=df, i=i)
+        print()
 
 
-# In[42]:
+# In[9]:
 
 
-pd.options.display.float_format = '{:,.3f}'.format
-df
+def print_df():
+    pd.options.display.float_format = '{:,.3f}'.format
+    df
 
