e6da010a5de37f99901acfa6d71f7c175fa77ef8
npool9 <npool@elon.edu>
2018-11-19 13:09:38 -0500

    Account for short ingredients and fix volume/mass

--- unit_conversion.py
+++ unit_conversion.py
@@ -3,101 +3,6 @@ import string
 import random
 
 
-def convert_to_ml(num, unit):
-    """
-    Convert any volume measurement from the current unit to milliliters
-
-    """
-    table = str.maketrans({key: None for key in string.punctuation})
-    unit = unit.translate(table).lower().strip()
-    the_unit = ""
-    
-    if "gal" in unit:
-        the_unit = "gallon"
-        weight = 3785.41
-    elif "qu" in unit or "qt" in unit:
-        the_unit = "quart"
-        weight = 946.353 
-    elif "pt" in unit or "pint" in unit:
-        the_unit = "pint"
-        weight = 473.176
-    elif "cup" in unit:
-        the_unit = "cup"
-        weight = 240
-    elif "fl" in unit or "oz" in unit:
-        the_unit = "fl oz"
-        weight = 29.5735
-    elif "tbsp" in unit or "table" in unit:
-        the_unit = "tablespoon"
-        weight = 14.7868
-    elif "tsp" in unit or "tea" in unit:
-        the_unit = "teaspoon"
-        weight = 4.92892
-    elif "m3" in unit or "cubic meter" in unit or "meter" in unit:
-        the_unit = "cubic meter"
-        weight = 1e6
-    elif "ml" in unit or "milli" in unit:
-        the_unit = "milliliter"
-        weight = 1
-    elif "liter" in unit or "l" == unit:
-        the_unit = "liter"
-        weight = 1000
-    elif "cubic foot" in unit or "f3" in unit:
-        the_unit = "cubic foot"
-        weight = 28316.8
-    elif "cubic inch" in unit or "in3" in unit:
-        the_unit = "cubic inch"
-        weight = 16.3871
-        
-    output = weight * num
-    print('Converted', the_unit, 'to milliliters')
-    return output
-
-
-def convert_to_grams(num, unit):
-    """
-    Convert any mass measurement to grams
-    :param num:
-    :param unit:
-    :return:
-    """
-    table = str.maketrans({key: None for key in string.punctuation})
-    unit = unit.translate(table).lower().strip()
-    the_unit = ""
-    
-    if "metric ton" in unit or "mt" in unit or "m t" in unit:
-        the_unit = "metric ton"
-        weight = 1e6
-    elif "kilo" in unit or "kg" in unit:
-        the_unit = "kilogram"
-        weight = 1000
-    elif "gram" in unit or "g" == unit:
-        the_unit = "gram"
-        weight = 1
-    elif "milli" in unit or "mg" in unit:
-        the_unit = "milligram"
-        weight = 0.001
-    elif "micro" in unit or "μg" in unit or "mcg" in unit:
-        the_unit = "microgram"
-        weight = 1e-6
-    elif "ton" in unit or "t" == unit:
-        the_unit = "ton"
-        weight = 907185
-    elif "stone" in unit or "st" == unit:
-        the_unit = "stone"
-        weight = 6350.29
-    elif "pound" in unit or "lb" in unit:
-        the_unit = "pound"
-        weight = 453.592
-    elif "ounce" in unit or "ozs" in unit or "oz" in unit:
-        the_unit = "ounces"
-        weight = 28.3495
-        
-    output = weight * num
-    print('Converted', the_unit, 'to grams')
-    return output
-
-
 def convert(num, unit, ingred):
     """
     Convert the unit, and let the code decide whether the measurement is volume or mass
@@ -109,94 +14,87 @@ def convert(num, unit, ingred):
     quantity = False
     
     # Testing for volume
-    if "gal " in unit or 'gallon ' in unit:
+    if "gal" in unit or 'gallon' in unit:
         the_unit = "gallon"
         weight = 3785.41
         volume = True
-    elif "quart " in unit or "qt " in unit or "q " in unit:
+    elif "quart" in unit or "qt" in unit or "q" in unit:
         the_unit = "quart"
         weight = 946.353
         volume = True
-    elif "pt " in unit or "pint " in unit:
+    elif "pt" in unit or "pint" in unit:
         the_unit = "pint"
         weight = 473.176
         volume = True
-    elif "cup " in unit or 'c ' == unit:
+    elif "cup" in unit or 'c' == unit:
         the_unit = "cup"
         weight = 240
         volume = True
-    elif "fl " in unit or "oz " in unit:
+    elif "fl" in unit or "oz" or "ounce" in unit:
         the_unit = "fl oz"
         weight = 29.5735
         volume = True
-    elif "tbsp " in unit or "table " in unit:
+    elif "tbsp" in unit or "table" in unit:
         the_unit = "tablespoon"
         weight = 14.7868
         volume = True
-    elif "tsp " in unit or "tea " in unit:
+    elif "tsp" in unit or "tea" in unit:
         the_unit = "teaspoon"
         weight = 4.92892
         volume = True
-    elif "m3 " in unit or "cubic meter " in unit or "meter " in unit:
+    elif "m3" in unit or "cubic meter" in unit or "meter" in unit:
         the_unit = "cubic meter"
         weight = 1e6
         volume = True
-    elif "ml " in unit or "milli " in unit:
+    elif "ml" in unit or "milli" in unit:
         the_unit = "milliliter"
         weight = 1
         volume = True
-    elif "liter " in unit or "l " == unit:
+    elif "liter" in unit or "l" == unit:
         the_unit = "liter"
         weight = 1000
         volume = True
-    elif "cubic foot " in unit or "f3 " in unit or "feet " in unit:
-        the_unit = "cubic foot"
-        weight = 28316.8
-        volume = True
-    elif "cubic inch " in unit or "in3 " in unit or "inch " in unit:
-        the_unit = "cubic inch"
-        weight = 16.3871
-        volume = True
-    elif "pinch " in unit or "bit " in unit: # extraneous cases
+    elif "pinch" in unit or "bit" or "drop" or "dash" in unit: # extraneous cases
         # Vocab words that we'll estimate to half a teaspoon
         the_unit = "teaspoon"
         num = 0.5
         weight = 4.92892
+        volume = True
       
     # Testing for mass
-    elif "metric ton" in unit or "mt " in unit or "m t " in unit:
+    elif "metric ton" in unit or "mt" in unit or "m t" in unit:
         the_unit = "metric ton"
         weight = 1e6
         mass = True
-    elif "kilo" in unit or "kg " in unit:
+    elif "kilo" in unit or "kg" in unit:
         the_unit = "kilogram"
         weight = 1000
         mass = True
-    elif "gram" in unit or "g " == unit:
+    elif "gram" in unit or "g" == unit:
         the_unit = "gram"
         weight = 1
         mass = True
-    elif "milli" in unit or "mg " in unit:
+    elif "milli" in unit or "mg" in unit:
         the_unit = "milligram"
         weight = 0.001
         mass = True
-    elif ("micro" in unit and "wave" not in unit) or "μg " in unit or "mcg " in unit:
+    elif ("micro" in unit and "wave" not in unit) or "μg" in unit or "mcg" in unit:
         the_unit = "microgram"
         weight = 1e-6
         mass = True
-    elif "ton " in unit or "t " == unit:
+    elif "ton" in unit or "t" == unit:
         the_unit = "ton"
         weight = 907185
         mass = True
-    elif "stone " in unit or "st " == unit:
+    elif "stone" in unit or "st" == unit:
         the_unit = "stone"
         weight = 6350.29
         mass = True
-    elif "pound " in unit or "lb " in unit:
+    elif "pound" in unit or "lb" in unit:
         the_unit = "pound"
         weight = 453.592
         mass = True
-    elif "ounce " in unit or "ozs " in unit or "oz " in unit:
+    elif "ounce" in unit or "ozs" in unit or "oz" in unit:
         the_unit = "ounces"
         weight = 28.3495
         mass = True
