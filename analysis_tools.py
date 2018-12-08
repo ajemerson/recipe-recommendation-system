db46ac3fb1d6e94d8fc98fa48fad6ef98ca5ce3e
Nathan Pool <nathanpool@nathans-mbp.home>
2018-12-08 15:40:59 -0500

    Add generalized DBSCAN and Silhouette Coefficient functions to analysis tools

--- analysis_tools.py
+++ analysis_tools.py
@@ -12,6 +12,8 @@ from sklearn.ensemble import ExtraTreesClassifier
 from keras.layers import Input, Dense
 from keras.models import Model
 from keras import losses
+from sklearn.metrics import silhouette_score as silhouette
+from sklearn.cluster import DBSCAN as dbizzle
 
 
 def draw_ellipse(position, covariance, ax=None, **kwargs):
@@ -163,4 +165,48 @@ def autoencoder(data, dim_enc1, dim_enc2):
     print('Saving encoded data...')
     enc_data.to_csv('recipes_encoded100.csv')
     print('Encoded data was successfully saved to a csv')
-    return(enc_data)
+    return enc_data
+
+
+def dbscan(data, eps, min_samps):
+    """
+    Perform DBSCAN density-based clustering on the given dataset
+    :param data: a pandas dataframe to be clustered
+    :param eps: hyperparameter of DBSCAN --- distance between points to be considered members of same neighborhood
+    :param min_samps: hyperparameter of DBSCAN --- number of neighbors for a observation to be considered core point
+    :return: the same pandas dataframe with clusters attached to each row
+    """
+
+    print('Clustering input data with DBSCAN...')
+    clustering = dbizzle(eps=eps, min_samples=min_samps).fit(data)
+    clusters = clustering.labels_
+    print('DBSCAN complete.')
+    print(clusters)
+    print("The unique clusters:", np.unique(clusters))
+
+    # count the number of noise points in the clustering
+    noise_count = 0
+    for i in range(len(clusters)):
+        if clusters[i] == -1:
+            noise_count += 1
+    print("Number of noise points:", noise_count)
+    silhouette_coef(data, clustering)
+
+    # append the clustering to the end of the input dataframe
+    data['clusters'] = clusters
+    return data
+
+
+def silhouette_coef(d, clustering):
+    """
+    Measure the performance of any clustering with Silhouette coefficient. Outputs a coefficient in [-1, 1]
+    We prefer coefficients close to 1
+    :param d: the pandas dataframe before appending cluster labels
+    :param clustering: the clustering of the data
+    :return coef: the Silhouette Coefficient
+    """
+    print("Calculating Silhouette Coefficient...")
+    labels = clustering.labels_
+    coef = silhouette(d, labels)
+    print('Silhouette Coefficient of clustering:', coef)
+    return coef
